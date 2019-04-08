488ec281b78cd57c07d6142b0b36b0e1c6983ac6
Law Yang <lawtouayang@gmail.com>
2019-04-08 14:51:55 -0500

    aws 2

--- server.js
+++ server.js
@@ -1,1 +1,15 @@
-import express from "express";
+const express = require('express');
+const app = express();
+
+// app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({ extended: true }));
+
+// Serve static files
+app.use(express.static('build'));
+
+// App Set //
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`Listening on port: ${PORT}`);
+  });
